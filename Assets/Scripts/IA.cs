ef4ed7905e6bdbbe82fb38842029fac452dbd192
Jorafe <joanrafe15@gmail.com>
2024-12-02 13:54:30 +0100

    puntos de patrulla

--- Assets/Scripts/IA.cs
+++ Assets/Scripts/IA.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class IA : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public enum EnemyState
     {
-        
+        Patrolling,
+        Chasing,
+        Searching
     }
 
+    public EnemyState currentState;
+
+    private NavMeshAgent agent;
+
+    [SerializeField] Transform[] patrolpoints;
+
     // Update is called once per frame
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()    
+    {
+        currentState = EnemyState.Patrolling;
+        SetRandomPatrolPoint();
+    }
+
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case EnemyState.Patrolling:
+                Patrol();
+                break;
+            case EnemyState.Chasing:
+                Chase();
+                break;
+            case EnemyState.Searching:
+                Searching();
+                break;
+        }
+
     }
+
+        void Patrol()
+        {
+            if(agent.remainingDistance < 0.2f)
+            {
+                SetRandomPatrolPoint();
+            }
+        }
+        void Chase()
+        {
+            
+        }
+        void Searching()
+        {
+
+        }
+
+        void SetRandomPatrolPoint()
+        {
+            agent.destination = patrolpoints[Random.Range(0, patrolpoints.Length)].position;
+        }
+    
 }
