e4c67e979e9123a420519b18b6f3a446a28cb42f
Jorafe <joanrafe15@gmail.com>
2024-12-02 13:11:39 +0100

    moviento

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire2"))
+        {
+            SetDestination();
+        }   
+    }
+
+    public void SetDestination()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if(hit.transform.gameObject.layer == 6)
+            {
+                agent.SetDestination(hit.point);    
+            }
+        }
+    }
+}
